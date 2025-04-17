3f8fe00eb1b9d0af3b0b4559f56d498c03cce22f
SOLV-Code <gpestal@solv.ca>
2025-04-17 15:38:12 -0700

    quarto reports started

--- CODE/1_Run_MetricsCalcs&RapidStatus.R
+++ CODE/1_Run_MetricsCalcs&RapidStatus.R
@@ -4,6 +4,18 @@
 # Use Ctrl-A to highlight whole script and then click "Run" above to run all the steps
 # Adjust True/False settings below as needed
 
+# IMPORTANT NOTES:
+# Running this code will overwrite the output files in your local copy of
+# the repository. If you change any settings or data, the revised plots will
+# be in the folder DATA/Dashboards
+# The very last step at the end, excluded with 'if(FALSE){}' is the function
+# that generates a basic quarto markdown template. If you run this step, it will overwrite
+# the current quarto markdown file which may have additional text that has been manually added after.
+# The quarto markdown can be used to render a word document. This was used as the starting point for
+# building the word documents in the EXAMPLES and SOLUTIONS folders.
+
+
+
 # Settings
 install.package <- TRUE # if TRUE, install the latest version of the R package
 
@@ -15,10 +27,10 @@ require("devtools") # check for and, if needed, install the devtools package
 library(devtools) # Load the devtools package.
 
 # for latest dev version, use this
-install_github("SOLV-CODE/WSP-Metrics-Pkg", dependencies = TRUE, build_vignettes = FALSE)
+#install_github("SOLV-CODE/WSP-Metrics-Pkg", dependencies = TRUE, build_vignettes = FALSE)
 
 # for latest official version, use this
-#install_github("Pacific-salmon-assess/WSP-Metrics-Pkg", dependencies = TRUE, build_vignettes = FALSE)
+install_github("Pacific-salmon-assess/WSP-Metrics-Pkg", dependencies = TRUE, build_vignettes = FALSE)
 
 
 library(WSPMetrics)
@@ -87,5 +99,13 @@ plotStatusDashboards(
   out.label = "RapidStatusOut",
   out.filepath = "DATA/Dashboards_NoStatus",
   plot.status.timeline = FALSE)
+
+
+
+
+generateReportTemplate(type = "quarto",
+                       file.label = "Report", # doesn't apply to readme
+                       files.path = "DATA",
+                       repo.path = NULL)
 
 
